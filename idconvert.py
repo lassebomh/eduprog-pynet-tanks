804f05761e0ad34b5b5d2c6f95876d83c75db909
Lasse Holmgaard Bomholt <lasse@bomh.net>
2020-05-15 10:11:33 +0200

    Created ID convert

---
+++ idconvert.py
@@ -0,0 +1,44 @@
+#
+#  Creating id's for tanks on the LAN.
+#
+#  Limits:
+#
+#    Your IP can range from 192.168.0.0 to 192.168.255.255
+#
+#    Your Tank ID can range from 0 to 24
+#
+#  Examples:
+#
+#    1.  IP 192.168.255.255, TANK 25      =  Z473
+#
+#    2.  IP 192.168.1.1, TANK 0           =  0075
+#
+#    3.  IP 192.168.2.235, TANK 10        =  E297
+#
+
+from baseconvert import base
+
+def basep(*args, pad=0, **kwargs): # Converts base AND pads the result.
+    out = base(*args, **kwargs)
+    return "0"*(pad-len(out))+out
+
+def getid(ip, tank):
+    ip1, ip2 = map(int, ip.split('.')[2:])
+
+    return basep(
+              basep(tank, 10, 16, string=True, pad=2) +
+              basep(ip1, 10, 16, string=True, pad=2) +
+              basep(ip2, 10, 16, string=True, pad=2)
+                 , 16, 36, string=True, pad=4)
+
+def fromid(id):
+    out = basep(id, 36, 16, string=True, pad=6)
+    result = tuple(map(lambda x: "".join(map(lambda y: str(y), base(x, 16, 10, string=True))), (out[2:4], out[4:6], out[0:2])))
+    return ('192.168.'+result[0]+"."+result[1], result[2])
+
+if __name__ == "__main__":
+
+    args = ("192.168.2.235", 10)
+
+    print(args, " ---> ", getid(*args), ' ---> ', fromid(getid(*args)))
+
